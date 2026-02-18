d516401b3425e48946baa7a7fb9cc030c34c9962
Sai Ram <sai@Mac.fios-router.home>
2026-02-18 13:18:33 -0500

    fix: added verify_token security

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
-Voice Obfuscation API – Enterprise Version
+**Voice Obfuscation API** | Sairam Ramakrishnan
+<br/>
+**IITM ID:** 24f2006201@ds.study.iitm.ac.in
 
-Live Demo:
-https://vo-xd2y.onrender.com
+
+**Live Demo**:
+https://vo-xd2y.onrender.com/docs - select obsfuscate, upload audio file
+*Note: First time the server may boot and could take couple of mins. Subsequent requests will be served in <5 seconds*
+
+**Metrics**:
+https://vo-xd2y.onrender.com/metrics
+
+*Grafana - not configured as Prometheus server could not be configured on time.*
 
 Features:
 
@@ -17,15 +26,12 @@ Tech Stack:
 
 FastAPI
 Docker
-AWS-ready
+github - CI/CD - Render
 Librosa
 Python
 
-
 Architecture:
-
 Client → API → Obfuscation Engine → Secure Output
-
 
 Run locally:
 
--- app/core/security.py
+++ app/core/security.py
@@ -1,20 +1,85 @@
-from jose import jwt
-from datetime import datetime, timedelta
-from app.core.config import settings
+"""
+Enterprise JWT Security
+"""
 
+from datetime import datetime, timedelta
+from jose import jwt, JWTError
+from fastapi import HTTPException, Security
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+
+SECRET_KEY = "CHANGE_THIS_TO_ENV_SECRET"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+
+security = HTTPBearer()
+
+
+# =========================
+# CREATE TOKEN
+# =========================
 
 def create_access_token(data: dict):
 
     to_encode = data.copy()
 
     expire = datetime.utcnow() + timedelta(
-        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+
+        minutes=ACCESS_TOKEN_EXPIRE_MINUTES
+
     )
 
-    to_encode.update({"exp": expire})
+    to_encode.update(
 
-    return jwt.encode(
+        {"exp": expire}
+
+    )
+
+    encoded_jwt = jwt.encode(
+
         to_encode,
-        settings.SECRET_KEY,
-        algorithm="HS256"
+
+        SECRET_KEY,
+
+        algorithm=ALGORITHM
+
     )
+
+    return encoded_jwt
+
+
+# =========================
+# VERIFY TOKEN
+# =========================
+
+def verify_token(
+
+    credentials: HTTPAuthorizationCredentials = Security(security)
+
+):
+
+    token = credentials.credentials
+
+    try:
+
+        payload = jwt.decode(
+
+            token,
+
+            SECRET_KEY,
+
+            algorithms=[ALGORITHM]
+
+        )
+
+        return payload
+
+    except JWTError:
+
+        raise HTTPException(
+
+            status_code=401,
+
+            detail="Invalid or expired token"
+
+        )
