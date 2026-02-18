00beebb211955a24be0abf6402b8fc96f6fbfffe
Sai Ram <sai@Mac.fios-router.home>
2026-02-18 12:08:59 -0500

    +prometheus

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from prometheus_client import Counter, generate_latest
+from fastapi.responses import Response
 
 from app.api.routes import router
 
@@ -11,3 +13,28 @@ app = FastAPI(
 
 
 app.include_router(router)
+
+REQUEST_COUNT = Counter(
+    "app_requests_total",
+    "Total requests"
+)
+
+
+@app.middleware("http")
+async def count_requests(request, call_next):
+
+    REQUEST_COUNT.inc()
+
+    response = await call_next(request)
+
+    return response
+
+
+@app.get("/metrics")
+def metrics():
+
+    return Response(
+        generate_latest(),
+        media_type="text/plain"
+    )
+
